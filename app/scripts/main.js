f3c1960c6da0498ee9477bb4f5d8f032eea4cb15
Michael Kuehn <mkuehn10@gmail.com>
2016-07-20 21:58:58 -0400

    add thumbnails and jquery ui theme

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,7 @@
 
     var tasteKidAPIKey = '232795-APIProje-WARMTIA1';
     // $('#projects').accordion();
-    $("#tabs").tabs();
+    $('#tabs').tabs();
     console.log(tastekid.Similar.Results);
 
     var results;// = tastekid.Similar.Results;
@@ -31,46 +31,51 @@
             results = result.Similar.Results;
             $.each(results, function(n, result) {
                 console.log(result.Name + ' is a ' + result.Type);
-                addAccordionToTabs(result);
+                addAccordionToTabs(n, result);
 
 
             });
             // console.log(result);
 
             // Setter
-            $( "#tabs" ).tabs( "option", "active", 0);
-            $('#accordion-music').accordion({
-                collapsible: true,
-                active: false
-            });
-            $('#accordion-movie').accordion({
-                collapsible: true,
-                active: false
-            });
-            $('#accordion-show').accordion({
-                collapsible: true,
-                active: false
-            });
-            $('#accordion-book').accordion({
-                collapsible: true,
-                active: false
-            });
-            $('#accordion-author').accordion({
-                collapsible: true,
-                active: false
-            });
-            $('#accordion-game').accordion({
-                collapsible: true,
-                active: false
-            });
+            $( '#tabs' ).tabs( 'option', 'active', 0);
+            // $('#accordion-music').accordion({
+            //     collapsible: true,
+            //     active: false
+            // });
+            // $('#accordion-movie').accordion({
+            //     collapsible: true,
+            //     active: false
+            // });
+            // $('#accordion-show').accordion({
+            //     collapsible: true,
+            //     active: false
+            // });
+            // $('#accordion-book').accordion({
+            //     collapsible: true,
+            //     active: false
+            // });
+            // $('#accordion-author').accordion({
+            //     collapsible: true,
+            //     active: false
+            // });
+            // $('#accordion-game').accordion({
+            //     collapsible: true,
+            //     active: false
+            // });
         })
         .fail(function(jqXHR, error) {
-            console.log("something went wrong");
+            console.log('something went wrong');
         })
     }
+//http://placehold.it/150x150
+    function addAccordionToTabs(n, result) {
+        // $('#accordion-' + result.Type).append('<h3>' + result.Name + '</h3><div><p>' + result.wTeaser + '</p></div>');
 
-    function addAccordionToTabs(result) {
-        $('#accordion-' + result.Type).append('<h3>' + result.Name + '</h3><div><p>' + result.wTeaser + '</p></div>');
+        $('#tabs-' + result.Type).append('<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 pic-container"><img class="img-responsive" src="http://placehold.it/150x150"><span class="caption">' + result.Name + '</span></div>');
+
+
+
         // $('#tabs-' + result.Type).append('<h3>' + result.Name + '</h3><div><p>' + result.wTeaser + '</p></div>');
 
     }
